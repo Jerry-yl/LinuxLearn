a1b8d8142a0cad3afd54dff0c4874890c056ec8a
Jerry <jerry.yl@foxmail.com>
2016-06-22 00:01:54 +0800

    1.编写了标准I/O的相关函数,并进行了练习!

--- SrcObject/main.c
+++ SrcObject/main.c
@@ -1,12 +1,42 @@
 
 #include "stdio.h"
+#include "string.h"
+#include "errno.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	int i;
+	int iLine = 0x12345678;
+	char acBuf[6] = {1,2,0,4,5,6};
+	char ca = 1;
+	char cb = 2;
+	char cc = 3;
+	char cd = 4;
+	int i = 0x12345678;
+	FILE *fp;
 
-	i = 1;
-	printf("This is first program.\n");
+//	if( argc < 2 )
+//	{
+//		printf("Usage: %s <file>.\n", argv);
+//		return (-1);
+//	}
+
+//	fp = fopen(argv[1], "r");
+//	if( NULL == fp )
+//	{
+//		perror("file to open:");
+//		return (-1);
+//	}
+
+//	while( fgets(acBuf, 1024, fp) != NULL)
+//	{
+//	//	if( acBuf[10])
+//		iLine++;
+//	}
+
+	printf("The array length is:%d.\n", strlen(acBuf));
+	acBuf[strlen(acBuf)-1] = 8;
+
+	printf("The file line is:%d.\n", acBuf[5]);
 	
 	return 0;
 }

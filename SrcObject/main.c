4574530a99315f3a68799d1b2677766c04cba61c
Jerry <jerry.yl@foxmail.com>
2016-06-24 00:07:09 +0800

    1.温故了一下这几天所学习的,重新根据几个小题目要求,进行了代码编写

--- SrcObject/main.c
+++ SrcObject/main.c
@@ -6,35 +6,56 @@
 
 int main(int argc, char *argv[])
 {
-	int iLine = 0;
+	int iGetResult = 0;
+	char *cp,tmp;
 	char acBuf[1024];
 	FILE *fps;
 	FILE *fpd;
 	time_t t;
-
+	cp = &tmp;
+	
 	if( argc < 2 )
 	{
 		printf("Usage: %s <file>.\n", argv[1]);
 		return (-1);
 	}
 
+	fps = fopen(argv[1], "r");
+	if( NULL == fps )
+	{
+		perror("fps file to open:");
+		return(-1);
+	}
+
 	fpd = fopen(argv[2], "w+");
 	if( NULL == fpd )
 	{
 		perror("Dest file to open:");
+		fclose(fps);
 		return (-1);
 	}
 
 	while( 1 )
 	{
-		time(&t);
-		fprintf(fpd, "%s\n", ctime(&t));
+//		time(&t);
+//		fprintf(fpd, "%s\n", ctime(&t));
+		cp = fgets(acBuf, sizeof(acBuf), fps);
+		if( NULL == cp )
+		{
+			perror("fgets is error");
+			break;
+		}
 //		fseek(fpd, 0, SEEK_CUR);
-//		sleep(1);
-		printf("%s\n",ctime(&t));
+		if( NULL == fputs(acBuf, fpd) )
+		{
+			perror("fputs is error");
+			break;
+		}
+		memset(acBuf, 0x00, sizeof(acBuf));
 	}
 
 	fclose(fpd);
+	fclose(fps);
 	
 	return 0;
 }

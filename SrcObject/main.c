3470e22df535a7d8d58debc6b66e57b3871efc91
Jerry <815115145@qq.com>
2016-07-20 17:55:31 +0800

    1.使用UDP编写了通讯示例程序

--- SrcObject/main.c
+++ SrcObject/main.c
@@ -18,11 +18,15 @@
 #include "Typedefs.h"
 #include "pthread.h"
 
-//#define SERV_PRO
+#define SERV_PRO
 
-#define SERV_PORT	  7000
-#define CLIENT_PORT 7001
-#define BACKLOG	  10
+#ifdef SERV_PRO
+#define SEND_PORT	  7456
+#define RECV_PORT   7123
+#else
+#define SEND_PORT	  7123
+#define RECV_PORT   7456
+#endif
 #define BUF_SIZE	  128
 
 void *pvRecvThread(void *pvArg)
@@ -30,12 +34,20 @@ void *pvRecvThread(void *pvArg)
 	char acBuf[BUF_SIZE];
 	int sockfd;
 	int ret;
-
+	struct sockaddr_in sock_addr;
+	uint32 u32AddrLen;
+	
 	sockfd = *((int *)pvArg);
+
+	sock_addr.sin_family = AF_INET;
+	sock_addr.sin_port = htons(SEND_PORT);
+	sock_addr.sin_addr.s_addr = INADDR_ANY;
+	bzero(&(sock_addr.sin_zero), 8);
+	u32AddrLen = sizeof(struct sockaddr_in);
 	
 	while(1)
 	{
-		ret = recv(sockfd, acBuf, BUF_SIZE, 0);
+		ret = recvfrom(sockfd, acBuf, BUF_SIZE, 0, (struct sockaddr *)&sock_addr, &u32AddrLen);
 		if( ret < 0 )
 		{
 			perror("recv error");
@@ -59,8 +71,15 @@ void *pvSendThreadFunc(void *pvArg)
 	char acBuf[BUF_SIZE];
 	int sockfd;
 	int ret;
-
+	struct sockaddr_in sock_addr;
+	
 	sockfd = *((int *)pvArg);
+
+	sock_addr.sin_family = AF_INET;
+	sock_addr.sin_port = htons(RECV_PORT);
+	sock_addr.sin_addr.s_addr = inet_addr("192.168.0.111");
+	bzero(&(sock_addr.sin_zero), 8);
+	
 	
 	while(1)
 	{
@@ -70,7 +89,7 @@ void *pvSendThreadFunc(void *pvArg)
 			perror("fgets error");
 			exit(1);
 		}
-		ret = send(sockfd, acBuf, BUF_SIZE, 0);
+		ret = sendto(sockfd, acBuf, BUF_SIZE, 0, (struct sockaddr *)&sock_addr, sizeof(struct sockaddr_in));
 		if( ret < 0 )
 		{
 			perror("send error");
@@ -94,16 +113,13 @@ int main(int argc, char *argv[])
 	int sock_fd;
 	int clients_fd;
 	struct sockaddr_in host_addr;
-	struct sockaddr_in server_addr;
-	struct sockaddr_in client_addr;
 	uint32 u32AddrLen;
 
 	int  iReuseAddr = TRUE;
-	int  clientPort;
 	pthread_t pthreadRecv_ID;
 	pthread_t pthreadSend_ID;
 
-	sock_fd = socket(AF_INET, SOCK_STREAM, 0);	
+	sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
 	if( sock_fd < 0)
 	{
 		perror("socket error");
@@ -113,7 +129,7 @@ int main(int argc, char *argv[])
 #ifdef SERV_PRO
 
 	host_addr.sin_family = AF_INET;
-	host_addr.sin_port = htons(SERV_PORT);
+	host_addr.sin_port = htons(SEND_PORT);
 	host_addr.sin_addr.s_addr = INADDR_ANY;  //本机IP地址
 	bzero(&(host_addr.sin_zero), 8);
 
@@ -124,42 +140,25 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	ret = listen(sock_fd, BACKLOG);
+	printf("Please waiting for the client connection.\n");
+
+	ret = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR,(char *)&iReuseAddr, sizeof(iReuseAddr));
 	if( ret < 0 )
 	{
-		perror("listen error");
+		perror("setsockopt error");
 		exit(1);
 	}
-
-	printf("Please waiting for the client connection.\n");
-	while(1)
+	ret = pthread_create(&pthreadRecv_ID, NULL, pvRecvThread, &sock_fd);
+	if( ret < 0 )
 	{
-		u32AddrLen = sizeof(struct sockaddr_in);
-		clients_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &u32AddrLen);
-		if( clients_fd < 0)
-		{
-			perror("accept error");
-			continue;
-		}
-		printf("Client IP: %s, PORT: %d.\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-		ret = setsockopt(clients_fd, SOL_SOCKET, SO_REUSEADDR,(char *)&iReuseAddr, sizeof(iReuseAddr));
-		if( ret < 0 )
-		{
-			perror("setsockopt error");
-			exit(1);
-		}
-		ret = pthread_create(&pthreadRecv_ID, NULL, pvRecvThread, &clients_fd);
-		if( ret < 0 )
-		{
-			perror("pthread_create error");
-			exit(1);
-		}
-		ret = pthread_create(&pthreadSend_ID, NULL, pvSendThreadFunc, &clients_fd);
-		if( ret < 0 )
-		{
-			perror("pthread_create error");
-			exit(1);
-		}
+		perror("pthread_create error");
+		exit(1);
+	}
+	ret = pthread_create(&pthreadSend_ID, NULL, pvSendThreadFunc, &sock_fd);
+	if( ret < 0 )
+	{
+		perror("pthread_create error");
+		exit(1);
 	}
 
 	pthread_join(pthreadRecv_ID, NULL);
@@ -167,14 +166,8 @@ int main(int argc, char *argv[])
 	
 #else
 
-	if( argc < 2 )
-	{
-		printf("a few argument.\n");
-		exit(1);
-	}
-	clientPort = atoi(argv[1]);
 	host_addr.sin_family = AF_INET;
-	host_addr.sin_port = htons(clientPort);
+	host_addr.sin_port = htons(SEND_PORT);
 	host_addr.sin_addr.s_addr = INADDR_ANY;  //本机IP地址
 	bzero(&(host_addr.sin_zero), 8);
 	ret = bind(sock_fd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr));
@@ -184,18 +177,6 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(SERV_PORT);
-//	server_addr.sin_addr.s_addr = INADDR_ANY;  //本机IP地址
-	server_addr.sin_addr.s_addr = inet_addr("192.168.0.111");  //本机IP地址
-	bzero(&(server_addr.sin_zero), 8);
-
-	ret = connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
-	if( ret < 0 )
-	{
-		perror("connect error");
-		exit(1);
-	}
 	ret = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR,(char *)&iReuseAddr, sizeof(iReuseAddr));
 	if( ret < 0 )
 	{
@@ -217,13 +198,10 @@ int main(int argc, char *argv[])
 	}
 	
 	pthread_join(pthreadRecv_ID, NULL);
-	pthread_join(pthreadSend_ID, NULL);
-	
+	pthread_join(pthreadSend_ID,NULL);
 #endif
 
 	close(sock_fd);
-	printf("program exit done.!\n");
-
 	return 0;
-}
 
+}

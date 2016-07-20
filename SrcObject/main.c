331055a2660c79c65a68293971936e9b2c00e38b
Jerry <815115145@qq.com>
2016-07-20 14:45:35 +0800

    1.学习了TCP编程，实现了客户端与服务端简单通信的目的

--- SrcObject/main.c
+++ SrcObject/main.c
@@ -4,6 +4,8 @@
 #include "unistd.h"
 #include "sys/stat.h"
 #include "sys/wait.h"
+#include "sys/socket.h"
+#include "arpa/inet.h"
 #include "fcntl.h"
 #include "syslog.h"
 #include "time.h"
@@ -16,58 +18,212 @@
 #include "Typedefs.h"
 #include "pthread.h"
 
-void *pvMyThread(void *pv)
-{
-	int i;
-	int ret;
-	
-	ret = pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);
-	if( ret != 0 )
-	{
-		perror("pthread_setcanceltype error");
-		exit(1);
-	}
-	
-	for(i=0;i<10;i++)
-	{
-		printf("----thread running.\n");
-		sleep(1);
-	}
-	
+//#define SERV_PRO
 
-	pthread_exit("Thank you for the cpu time.\n");
-	return pv;
+#define SERV_PORT	  7000
+#define CLIENT_PORT 7001
+#define BACKLOG	  10
+#define BUF_SIZE	  128
+
+void *pvRecvThread(void *pvArg)
+{
+	char acBuf[BUF_SIZE];
+	int sockfd;
+	int ret;
+
+	sockfd = *((int *)pvArg);
+	
+	while(1)
+	{
+		ret = recv(sockfd, acBuf, BUF_SIZE, 0);
+		if( ret < 0 )
+		{
+			perror("recv error");
+			exit(1);
+		}
+		printf("Recv msg:%s\n", acBuf);
+		if( 0 == strncmp(acBuf, "exit", 4) )
+		{
+			printf("TCP exit.\n");
+			break;
+		}
+
+		usleep(10000);
+	}
+	
+	pthread_exit(NULL);
+}
+
+void *pvSendThreadFunc(void *pvArg)
+{
+	char acBuf[BUF_SIZE];
+	int sockfd;
+	int ret;
+
+	sockfd = *((int *)pvArg);
+	
+	while(1)
+	{
+		printf("please input msg:");
+		if(NULL == fgets(acBuf, sizeof(acBuf), stdin))
+		{
+			perror("fgets error");
+			exit(1);
+		}
+		ret = send(sockfd, acBuf, BUF_SIZE, 0);
+		if( ret < 0 )
+		{
+			perror("send error");
+			exit(1);
+		}
+		if( 0 == strncmp(acBuf, "exit", 4) )
+		{
+			printf("TCP exit.\n");
+			break;
+		}
+
+		usleep(10000);
+	}
+	
+	pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
-	pthread_t pthreadID;
 	int ret;
-	int i;
-	void *pthread_result = "I like you";
-	ret = pthread_create(&pthreadID, NULL, pvMyThread, NULL);
-	if( ret != 0 )
+	int sock_fd;
+	int clients_fd;
+	struct sockaddr_in host_addr;
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+	uint32 u32AddrLen;
+
+	int  iReuseAddr = TRUE;
+	int  clientPort;
+	pthread_t pthreadRecv_ID;
+	pthread_t pthreadSend_ID;
+
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0);	
+	if( sock_fd < 0)
 	{
-		perror("++++pthread_create error");
+		perror("socket error");
 		exit(1);
 	}
 
-//	for(i=0;i<5;i++)
-//	{
-//		printf("Process running.\n");
-//		sleep(1);
-//	}
-	sleep(3);
-	ret = pthread_cancel(pthreadID);
-	if( ret != 0 )
+#ifdef SERV_PRO
+
+	host_addr.sin_family = AF_INET;
+	host_addr.sin_port = htons(SERV_PORT);
+	host_addr.sin_addr.s_addr = INADDR_ANY;  //本机IP地址
+	bzero(&(host_addr.sin_zero), 8);
+
+	ret = bind(sock_fd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr));
+	if( ret < 0 )
 	{
-		perror("pthread_cancel error");
+		perror("bind error");
 		exit(1);
 	}
 
-	pthread_join(pthreadID, &pthread_result);
-	printf("Thread join,it returned:%s\n",(char *)pthread_result);
+	ret = listen(sock_fd, BACKLOG);
+	if( ret < 0 )
+	{
+		perror("listen error");
+		exit(1);
+	}
+
+	printf("Please waiting for the client connection.\n");
+	while(1)
+	{
+		u32AddrLen = sizeof(struct sockaddr_in);
+		clients_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &u32AddrLen);
+		if( clients_fd < 0)
+		{
+			perror("accept error");
+			continue;
+		}
+		printf("Client IP: %s, PORT: %d.\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+		ret = setsockopt(clients_fd, SOL_SOCKET, SO_REUSEADDR,(char *)&iReuseAddr, sizeof(iReuseAddr));
+		if( ret < 0 )
+		{
+			perror("setsockopt error");
+			exit(1);
+		}
+		ret = pthread_create(&pthreadRecv_ID, NULL, pvRecvThread, &clients_fd);
+		if( ret < 0 )
+		{
+			perror("pthread_create error");
+			exit(1);
+		}
+		ret = pthread_create(&pthreadSend_ID, NULL, pvSendThreadFunc, &clients_fd);
+		if( ret < 0 )
+		{
+			perror("pthread_create error");
+			exit(1);
+		}
+	}
+
+	pthread_join(pthreadRecv_ID, NULL);
+	pthread_join(pthreadSend_ID, NULL);
 	
+#else
+
+	if( argc < 2 )
+	{
+		printf("a few argument.\n");
+		exit(1);
+	}
+	clientPort = atoi(argv[1]);
+	host_addr.sin_family = AF_INET;
+	host_addr.sin_port = htons(clientPort);
+	host_addr.sin_addr.s_addr = INADDR_ANY;  //本机IP地址
+	bzero(&(host_addr.sin_zero), 8);
+	ret = bind(sock_fd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr));
+	if( ret < 0 )
+	{
+		perror("bind error");
+		exit(1);
+	}
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(SERV_PORT);
+//	server_addr.sin_addr.s_addr = INADDR_ANY;  //本机IP地址
+	server_addr.sin_addr.s_addr = inet_addr("192.168.0.111");  //本机IP地址
+	bzero(&(server_addr.sin_zero), 8);
+
+	ret = connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
+	if( ret < 0 )
+	{
+		perror("connect error");
+		exit(1);
+	}
+	ret = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR,(char *)&iReuseAddr, sizeof(iReuseAddr));
+	if( ret < 0 )
+	{
+		perror("setsockopt error");
+		exit(1);
+	}
+
+	ret = pthread_create(&pthreadSend_ID, NULL, pvSendThreadFunc, &sock_fd);
+	if( ret < 0 )
+	{
+		perror("pthread_create error");
+		exit(1);
+	}
+	ret = pthread_create(&pthreadRecv_ID, NULL, pvRecvThread, &sock_fd);
+	if( ret < 0 )
+	{
+		perror("pthread_create error");
+		exit(1);
+	}
+	
+	pthread_join(pthreadRecv_ID, NULL);
+	pthread_join(pthreadSend_ID, NULL);
+	
+#endif
+
+	close(sock_fd);
+	printf("program exit done.!\n");
+
 	return 0;
 }
 
--- SrcObject/main.d
+++ SrcObject/main.d
@@ -21,7 +21,13 @@ main.o: main.c /usr/include/stdio.h /usr/include/features.h \
  /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
  /usr/include/unistd.h /usr/include/bits/posix_opt.h \
  /usr/include/bits/confname.h /usr/include/getopt.h \
- /usr/include/sys/stat.h /usr/include/bits/stat.h /usr/include/fcntl.h \
+ /usr/include/sys/stat.h /usr/include/bits/stat.h \
+ /usr/include/sys/socket.h /usr/include/sys/uio.h /usr/include/bits/uio.h \
+ /usr/include/bits/socket.h /usr/include/bits/sockaddr.h \
+ /usr/include/asm/socket.h /usr/include/asm-generic/socket.h \
+ /usr/include/asm/sockios.h /usr/include/asm-generic/sockios.h \
+ /usr/include/arpa/inet.h /usr/include/netinet/in.h /usr/include/stdint.h \
+ /usr/include/bits/wchar.h /usr/include/bits/in.h /usr/include/fcntl.h \
  /usr/include/bits/fcntl.h /usr/include/syslog.h \
  /usr/include/sys/syslog.h /usr/include/bits/syslog-path.h \
  /usr/include/xlocale.h /usr/include/string.h /usr/include/errno.h \
@@ -31,8 +37,7 @@ main.o: main.c /usr/include/stdio.h /usr/include/features.h \
  /usr/include/bits/ipctypes.h /usr/include/bits/ipc.h \
  /usr/include/sys/msg.h /usr/include/bits/msq.h /usr/include/sys/sem.h \
  /usr/include/bits/sem.h /usr/include/sys/shm.h /usr/include/bits/shm.h \
- Typedefs.h /usr/include/stdint.h /usr/include/bits/wchar.h \
- /usr/include/rpc/types.h /usr/include/sys/time.h \
+ Typedefs.h /usr/include/rpc/types.h /usr/include/sys/time.h \
  /usr/include/sys/param.h \
  /usr/lib/gcc/i486-linux-gnu/4.4.3/include-fixed/limits.h \
  /usr/lib/gcc/i486-linux-gnu/4.4.3/include-fixed/syslimits.h \
@@ -40,13 +45,8 @@ main.o: main.c /usr/include/stdio.h /usr/include/features.h \
  /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
  /usr/include/bits/posix2_lim.h /usr/include/linux/param.h \
  /usr/include/asm/param.h /usr/include/asm-generic/param.h \
- /usr/include/netinet/in.h /usr/include/sys/socket.h \
- /usr/include/sys/uio.h /usr/include/bits/uio.h \
- /usr/include/bits/socket.h /usr/include/bits/sockaddr.h \
- /usr/include/asm/socket.h /usr/include/asm-generic/socket.h \
- /usr/include/asm/sockios.h /usr/include/asm-generic/sockios.h \
- /usr/include/bits/in.h /usr/include/pthread.h /usr/include/sched.h \
- /usr/include/bits/sched.h /usr/include/bits/setjmp.h
+ /usr/include/pthread.h /usr/include/sched.h /usr/include/bits/sched.h \
+ /usr/include/bits/setjmp.h
 
 /usr/include/stdio.h:
 
@@ -144,6 +144,34 @@ main.o: main.c /usr/include/stdio.h /usr/include/features.h \
 
 /usr/include/bits/stat.h:
 
+/usr/include/sys/socket.h:
+
+/usr/include/sys/uio.h:
+
+/usr/include/bits/uio.h:
+
+/usr/include/bits/socket.h:
+
+/usr/include/bits/sockaddr.h:
+
+/usr/include/asm/socket.h:
+
+/usr/include/asm-generic/socket.h:
+
+/usr/include/asm/sockios.h:
+
+/usr/include/asm-generic/sockios.h:
+
+/usr/include/arpa/inet.h:
+
+/usr/include/netinet/in.h:
+
+/usr/include/stdint.h:
+
+/usr/include/bits/wchar.h:
+
+/usr/include/bits/in.h:
+
 /usr/include/fcntl.h:
 
 /usr/include/bits/fcntl.h:
@@ -190,10 +218,6 @@ main.o: main.c /usr/include/stdio.h /usr/include/features.h \
 
 Typedefs.h:
 
-/usr/include/stdint.h:
-
-/usr/include/bits/wchar.h:
-
 /usr/include/rpc/types.h:
 
 /usr/include/sys/time.h:
@@ -219,28 +243,6 @@ Typedefs.h:
 /usr/include/asm/param.h:
 
 /usr/include/asm-generic/param.h:
-
-/usr/include/netinet/in.h:
-
-/usr/include/sys/socket.h:
-
-/usr/include/sys/uio.h:
-
-/usr/include/bits/uio.h:
-
-/usr/include/bits/socket.h:
-
-/usr/include/bits/sockaddr.h:
-
-/usr/include/asm/socket.h:
-
-/usr/include/asm-generic/socket.h:
-
-/usr/include/asm/sockios.h:
-
-/usr/include/asm-generic/sockios.h:
-
-/usr/include/bits/in.h:
 
 /usr/include/pthread.h:
 
